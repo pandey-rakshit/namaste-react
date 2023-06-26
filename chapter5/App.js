bed38174703e3721796eab90aa1d52871f454c74
pandey-rakshit <pandeyrakshit03@gmail.com>
2023-06-26 09:54:17 +0530

    ADD: chapter5 - Ignite App - Initial commit

---
+++ chapter5/App.js
@@ -0,0 +1,23 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", { id: "heading" }, "Hello World from React"),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+]);
+
+console.log(heading); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
---
+++ chapter5/readme.md
@@ -0,0 +1,11 @@
+# Chapter 5 - Ignite our app
+
+In [`chapter 4`](../chapter4/readme.md),
+
+we refactored our code and reason why its important.
+
+Now in this lecture we will see integrity of the app and how we will ignite the app without using `npx create-react-app my-project` command
+
+> **NOTE** using `npx create-react-app my-project` will create a `project` with `my-project` name which is already ignited means there is all the things are done or it is runnable.
+
+> **NOTE** npm is a package manager but it does not stand for node package manager.
